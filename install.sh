9c83f9892528a7cab2aa133b75691b1205103a47
Rusty Hann <github@rustyhann.com>
2020-04-27 15:31:52 -0500

    still working

--- install.sh
+++ install.sh
@@ -13,23 +13,26 @@ parted /dev/nvme0n1 -- mkpart primary ext4 3.5GiB 100%
 
 # Documentation has 'boot'
 # Will throw an error if 'boot' instead of 'BOOT'
+# Needs to be BOOT to be DOS and Windows compatible
+# BOOT will also supress the warning thrown about
+# DOS and Windows compatibility
 mkfs.fat -F 32 -n BOOT /dev/nvme0n1p1
 
 mkswap -L swap /dev/nvme0n1p2
 mkfs.ext4 -L nixos /dev/nvme0n1p3
 mount /dev/disk/by-label/nixos /mnt
 
-# Documentation has 'boot'
-# change this to 'BOOT'
+# Must be /boot, lowercase, so the installation
+# doesn't fail
 mkdir -p /mnt/boot
-
-# Documentation has 'boot'
-# This will error, needs to be 'BOOT'
 mount /dev/disk/by-label/BOOT /mnt/boot
 
 swapon /dev/nvme0n1p2
 nixos-generate-config --root /mnt
 
 # Edit the config as needed
-# nano /mnt/etc/nixos/configuration.nix
+nano /mnt/etc/nixos/configuration.nix
 
+nixos-install
+
+reboot
